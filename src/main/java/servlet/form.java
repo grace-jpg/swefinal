07ee8acab14c602a4f0333d2a76f0808a59eb07e
Grace Jang <gracejang0109@gmail.com>
2020-05-19 23:44:54 -0400

    test

--- src/main/java/servlet/form.java
+++ src/main/java/servlet/form.java
@@ -9,7 +9,6 @@ import java.lang.*;
 
 @WebServlet(name = "form", urlPatterns = {"/form"})
 public class form extends HttpServlet {
-    
  
     
    /* A recursive algorithm to print a truth table of 1s and 0s.
@@ -36,11 +35,14 @@ public class form extends HttpServlet {
 
     public void doPost (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
       
+        
+        String input = request.getParameter("input");
         response.setContentType("text/html");
         PrintWriter out = response.getWriter(); 
-        String s = request.getParameter("input");
-//ArrayList s = request.getParameter("input");
-        String[] data = s.split(" ");
+        printhead(out);
+
+        //ArrayList s = request.getParameter("input");
+        String[] data = input.split(" ");
 
         /* examples:
          * A OR B
@@ -48,79 +50,87 @@ public class form extends HttpServlet {
          * M and N or Q
          * today | tomorrow
          */
-        String[] variables = new String[data.length];
-        String[] operators = new String[data.length];
+
+        ArrayList <String> variables;
+        ArrayList <String> operators;
+        // String[] variables = new String[data.length];
+        // String[] operators = new String[data.length];
         String[] or = {"or", "OR", "|"};
         String[] and = {"and", "AND", "&&"};
 
         int count1 = 0;
         int count2 = 0;
-        int andop;
 
         for (int num = 0; num < data.length; num++) {
             // if even, index (therefore variable)
             if (num % 2 == 0) 
-                variables[count1++] = data[num];
+                variables.set(count1++, data[num]);
 
             // else, operator
             else 
-                operators[count2++] = data[num];
+                operators.set(count2++, data[num]);
         }
 
-        if (or.contains(operator[0]))
-            andop = 0;
+        // if (or.contains(operator[0]))
+        //     andop = 0;
 
-        else if (and.contains(operator[0]))
-            andop = 1;
+        // else if (and.contains(operator[0]))
+        //     andop = 1;
 
-        else
-            out.print("invalid input");
+        // else
+        //     out.print("invalid input");
     
+        printtable(variables, operators);
 
-
-
+        out.print("</html>");
         out.close();
     }
+
+
 
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
+        printhead(out);
+        
+        out.print("<p>please enter a boolean predicate that has boolean variables </p>");
+        out.print("<p> and logical operators: </p>");
+
+        String input = request.getParameter("input");
 
         // prints <head>
-        out.print("<html>\n<head>\n\n");
-        out.print("<title>Truth Table</title>\n");
-        out.print("</head>\n");
-
+    
+        printhead(out);
         out.print("<body>\n");
         out.print("<form method=\"post\"");
 
         out.print("<center><h2>Truth Table</h2></center>\n");
-//        out.print("<p>Given predicate: " + fname + "</p>");
-
-//      String Nm = request.getParameter("predicate");
+//      out.print("<p>Given predicate: " + fname + "</p>");
 
         out.println("<table width=\"50%\" border=\"1\" align=\"center\">");
 
         for (int a : count1)
-                out.println("<th> " + variables[a] + "");
-
-        // Enumeration headerNames = request.getHeaderNames();
-        // while (headerNames.hasMoreElements()) {
-        //     String headerName = (String) headerNames.nextElement();
-        //     out.println(" <tr><td>" + headerName);
-        //     out.println("     <td>" + request.getHeader (headerName) + "</tr>");
-        // }
-   
+                out.println("<th> " + variables[a] + "");   
    
         out.println ("</table>");
         out.print("</body>\n");
-        out.print("</html>\n");
+
+ 
+        out.print("</html>");
         out.close ();
+    }
+
+    public void printtable (ArrayList <String> variables, ArrayList <String> operators) {
 
     }
 
+    public void printhead (Printwriter out) {
+        out.print("<html>\n<head>\n\n");
+        out.print("<title>Truth Table</title>\n");
+        out.print("</head>\n");
+    }
 
 
 }
